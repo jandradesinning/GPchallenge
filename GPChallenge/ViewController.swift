b8306ee8a354e5f50dd47707c0d213aa2c6f5872
Jose F. Andrade-Sinning <jandradesinning@gmail.com>
2019-03-23 23:00:40 -0400

    initial GUI components

--- GPChallenge/ViewController.swift
+++ GPChallenge/ViewController.swift
@@ -7,14 +7,129 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let tableVIew: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = false
+        return tableView
+    }()
+    
+    let albumCellId = "albumCellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
+        setupTableView()
+        
     }
 
+    func setupViews() {
+        navigationItem.title = "Apple Music - iTunes"
+        navigationController?.navigationBar.barTintColor = UIColor(r: 0, g: 255, b: 198)
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.darkGray,
+                                                                   NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
+    }
+    
+    func setupTableView() {
+        tableVIew.delegate = self
+        tableVIew.dataSource = self
+        tableVIew.register(BandCell.self, forCellReuseIdentifier: albumCellId)
+        
+        view.addSubview(tableVIew)
+        tableVIew.setAnchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: albumCellId, for: indexPath) as! BandCell
+        //cell.pictureImageView.image = artisArtworkArray[indexPath.item]
+        //cell.titleLabel.text = AlbumsArray[indexPath.item] + " - " + artistsArray[indexPath.item]
+        
+        
+        if indexPath.section == 1 {
+         //   cell.pictureImageView.image = UIImage(named: songsArray[indexPath.item].image!)
+         //   cell.titleLabel.text = artistsArray[indexPath.item]
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        //if section == 1 {
+        //    return "Top Artists"
+        //}
+        return "Albums - Coming Soon"
+    }
+    
+}
+
+class BandCell: UITableViewCell {
+    
+    let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.setCellShadow()
+        return view
+    }()
+    
+    let pictureImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    func setup() {
+        backgroundColor = UIColor(r: 245, g: 245, b: 245)
+        addSubview(cellView)
+        cellView.addSubview(pictureImageView)
+        cellView.addSubview(titleLabel)
+        
+        cellView.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 8, paddingBottom: 4, paddingRight: 8)
+        
+        pictureImageView.setAnchor(top: nil, left: cellView.leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        pictureImageView.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        
+        titleLabel.setAnchor(top: nil, left: pictureImageView.rightAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, height: 40)
+        titleLabel.centerYAnchor.constraint(equalTo: pictureImageView.centerYAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
